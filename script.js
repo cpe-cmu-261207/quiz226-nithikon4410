dc4edd07daef566dcee8d3da9cf99751c7c0fe35
nithikon4410 <76035304+nithikon4410@users.noreply.github.com>
2021-08-05 14:58:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-const author = document.querySelector("#author")
-const calculation = document.querySelector("#calculation")
-const btn_toggle = document.querySelector("#toggle");
-const length = document.getElementById('length')
-const color = document.querySelector('#color')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js" defer></script>
+  <title>Document</title>
+</head>
+<body>
+  
+  <div>
+    <h2 id="author">640631102 PANNAWIT PANWONG</h2>
+    <h2 id="calculation"></h2>
+    <button class="button" id="toggle">Show Calculation</button>
+  </div>
+  
+  <hr/>
 
-// define more constants and variables here
+  <p id="text">Hey, you're not permitted in there. It's restricted. You'll be deactivated for sure.. Don't call me a mindless philosopher, you overweight glob of grease! Now come out before somebody sees you. Secret mission? What plans? What are you talking about? I'm not getting in there! I'm going to regret this. There goes another one. Hold your fire. There are no life forms. It must have been short-circuited. That's funny, the damage doesn't look as bad from out here. Are you sure this things safe?
 
-btn_toggle.onclick = () => {
-  // your code here
-}
+  Close up formation. You'd better let her loose. Almost there! I can't hold them! It's away! It's a hit! Negative. Negative! It didn't go in. It just impacted on the surface. Red Leader, we're right above you. Turn to point... oh-five, we'll cover for you. Stay there... I just lost my starboard engine. Get set to make your attack run.
 
-const search =document.getElementById('search')
-const reset = document.getElementById('reset')
-const text = document.getElementById('text')
+  The Death Star plans are not in the main computer. Where are those transmissions you intercepted? What have you done with those plans? We intercepted no transmissions. Aaah....This is a consular ship. Were on a diplomatic mission. If this is a consular ship...were is the Ambassador? Commander, tear this ship apart until you've found those plans and bring me the Ambassador. I want her alive! There she is! Set for stun! She'll be all right. Inform Lord Vader we have a prisoner.
 
+  What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel.
 
+  Run, Luke! Run!</p>
 
-let state=false
-let textOg=text.innerText
-search.onclick= ()=> {
-  let array =text.innerText.split(' ')
-  for(let i=0;i<array.length;i++){
-    if(array[i].length>=length.value){
-      array[i]= "<span style= 'color:"+ color.value +"'>" +array[i] +"</span>"
-    }
-  }
-  const fusion=array.join(' ')
-  text.innerHTML=fusion
-  state=true
-}
+  <hr/>
 
-color.oninput =()=>{
-  if(state==true){
-    let array =text.innerText.split(' ')
-    for(let i=0;i<array.length;i++){
-    if(array[i].length>=length.value){
-      array[i]= "<span style= 'color:"+ color.value +"'>" +array[i] +"</span>"
-    }
-  }
-  const fusion=array.join(' ')
-  text.innerHTML=fusion
-  }
-}
+  <div>
+    #Characters to search: 
+    <input type="number" id="length" name="length" value=5>
+      
+    Text color: 
+    <input type="color" id="color" name="color" value="#FF0000">
+    
+    <br/>
+    <button id="search">Search</button>
+    <button id="reset">Reset</button>
+    </div>
 
-reset.onclick=()=>{
-    length.value=5
-    text.innerHTML=textOg
-    color.value="#FF0000"
-}
+</body>
+</html>
